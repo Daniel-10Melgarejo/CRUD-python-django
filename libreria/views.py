9a19abd422bd55e0fc5f461a2b88e0cd755a8c04
Daniel-10Melgarejo <oscar95d.melgarejo@gmail.com>
2024-04-25 16:37:28 -0400

    Creando el crud

--- libreria/views.py
+++ libreria/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1>Bienvenido a tu primera Web con Django</h1") # recibe una respuesta
+    return render(request, 'paginas/inicio.html') # recibe una respuesta
 def nosotros(request):
     return render(request, 'paginas/nosotros.html') #renderiza la página para utilizar y mostrar
+def libros(request):
+    return render(request, 'libros/index.html') #renderiza la página para utilizar y mostrar}
+def crear_libros(request):
+    return render(request, 'libros/crear.html') #renderiza la página para utilizar y mostrar
