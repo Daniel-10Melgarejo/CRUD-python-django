e52beb7e340050e3d71b2fcece1c37676c6780ee
Daniel <oscar95d.melgarejo@gmail.com>
2024-04-24 20:13:47 -0400

    Creación html para libros

--- libreria/views.py
+++ libreria/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse("<h1>Bienvenido a tu primera Web con Django</h1")
+    return HttpResponse("<h1>Bienvenido a tu primera Web con Django</h1") # recibe una respuesta
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html') #renderiza la página para utilizar y mostrar
