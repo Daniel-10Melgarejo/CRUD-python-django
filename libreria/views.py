14420ed18e1490d5cf4132cd586ccdbf524ef38f
Daniel-10Melgarejo <oscar95d.melgarejo@gmail.com>
2024-04-30 14:43:53 -0400

    Crud

--- libreria/views.py
+++ libreria/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Libro
 # Create your views here.
 
 def inicio(request):
     return render(request, 'paginas/inicio.html') # recibe una respuesta
+
 def nosotros(request):
     return render(request, 'paginas/nosotros.html') #renderiza la página para utilizar y mostrar
+
 def libros(request):
-    return render(request, 'libros/index.html') #renderiza la página para utilizar y mostrar
+    libros = Libro.objects.all()
+    return render(request, 'libros/index.html', {'libros': libros}) #renderiza la página para utilizar y mostrar
+
 def crear_libros(request):
     return render(request, 'libros/crear.html') #renderiza la página para utilizar y mostrar
+
 def editar_libros(request):
     return render(request, 'libros/editar.html') #renderiza la página para utilizar y mostrar
